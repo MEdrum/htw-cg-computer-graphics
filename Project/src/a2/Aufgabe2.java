52d7136eb2ec8fb44d3abb0e4888de742e9e8fbb
Fabian Backhaus <Fabian.Backhaus@Student.HTW-Berlin.de>
2025-11-04 12:21:30 +0100

    feat: add vertex shader rotation

--- Project/src/a2/Aufgabe2.java
+++ Project/src/a2/Aufgabe2.java
@@ -46,15 +46,15 @@ public class Aufgabe2 extends AbstractOpenGLBase {
         int vboId = glGenBuffers();
         glBindBuffer(GL_ARRAY_BUFFER, vboId);
         glBufferData(GL_ARRAY_BUFFER, triangles, GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0);
-        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0); // connect VBO
+        glEnableVertexAttribArray(0); // activate VBO
 
         // transfer colors to gpu
         int colorVboId = glGenBuffers();
         glBindBuffer(GL_ARRAY_BUFFER, colorVboId);
         glBufferData(GL_ARRAY_BUFFER, colors, GL_STATIC_DRAW);
-        glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
-        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0); // connect VBO
+        glEnableVertexAttribArray(1); // activate VBO
 
 	}
 
