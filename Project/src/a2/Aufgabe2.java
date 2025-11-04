3010eaa95217012373333908740792c733775599
Fabian Backhaus <Fabian.Backhaus@Student.HTW-Berlin.de>
2025-11-04 11:40:49 +0100

    feat: draw triangles in single color#

--- Project/src/a2/Aufgabe2.java
+++ Project/src/a2/Aufgabe2.java
@@ -18,6 +18,18 @@ public class Aufgabe2 extends AbstractOpenGLBase {
 		glUseProgram(shaderProgram.getId());
 
 		// Koordinaten, VAO, VBO, ... hier anlegen und im Grafikspeicher ablegen
+        float [] triangles = new float[]{
+                -0.5f, -0.5f, -0.5f, 0.45f, 0.45f, -0.5f, //Dreieck 0  [x0, y0, x1, y1, x2, y2]
+                0.5f, 0.5f, 0.5f, -0.45f, -0.45f, 0.5f //Dreieck 1
+            };
+
+        int vaoId = glGenVertexArrays();
+        glBindVertexArray(vaoId);
+        int vboId = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboId);
+        glBufferData(GL_ARRAY_BUFFER, triangles, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0);
+        glEnableVertexAttribArray(0);
 	}
 
 	@Override
@@ -29,5 +41,6 @@ public class Aufgabe2 extends AbstractOpenGLBase {
 		glClear(GL_COLOR_BUFFER_BIT); // Zeichenflaeche leeren
 
 		// hier vorher erzeugte VAOs zeichnen
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 	}
 }
--- Project/src/res/shaders/aufgabe2_v.glsl
+++ Project/src/res/shaders/aufgabe2_v.glsl
@@ -1,1 +1,7 @@
 #version 330
+
+layout(location=0) in vec2 triangles;
+
+void main(){
+    gl_Position = vec4(triangles, 0.0, 1.0);
+}
