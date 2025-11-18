866fb227c4bdd7940030f801658a9b3a8e4f41b7
Fabian Backhaus <Fabian.Backhaus@Student.HTW-Berlin.de>
2025-11-18 12:01:12 +0100

    implement Matrix4 class

--- Project/src/projekt/Matrix4.java
+++ Project/src/projekt/Matrix4.java
@@ -1,59 +1,145 @@
 package projekt;
 
-//Alle Operationen aendern das Matrixobjekt selbst und geben das eigene Matrixobjekt zurueck
-//Dadurch kann man Aufrufe verketten, z.B.
+//All operations modify the matrix object itself, and return this object as well.
+//Chaining of method-calls:
 //Matrix4 m = new Matrix4().scale(5).translate(0,1,0).rotateX(0.5f);
 public class Matrix4 {
+    float[][] values;
 
 	public Matrix4() {
-		// TODO mit der Identitaetsmatrix initialisieren
+		// init with identity
+        values = new float[][] {
+                {1f, 0f, 0f, 0f},
+                {0f, 1f, 0f, 0f},
+                {0f, 0f, 1f, 0f},
+                {0f, 0f, 0f, 1f},
+        };
 	}
 
-	public Matrix4(Matrix4 copy) {
-		// TODO neues Objekt mit den Werten von "copy" initialisieren
+	public Matrix4(Matrix4 original) {
+		// copy-constructor
+        this.values = new float[4][4];
+        for (int i = 0; i < 4; i++) {
+            System.arraycopy(original.values[i], 0, this.values[i], 0, 4);
+        }
 	}
 
 	public Matrix4(float near, float far) {
-		// TODO erzeugt Projektionsmatrix mit Abstand zur nahen Ebene "near" und Abstand zur fernen Ebene "far", ggf. weitere Parameter hinzufuegen
+		// creates a projection matrix
+
+        float frac1 = (-far - near) / (far - near);
+        float frac2 = (-2*near*far) / (far - near);
+        this.values = new float[][] {
+                {1f, 0f, 0f,    0f},
+                {0f, 1f, 0f,    0f},
+                {0f, 0f, frac1, frac2},
+                {0f, 0f, -1f,   0f}
+        };
 	}
 
+    public Matrix4(float near, float far, float width, float height) {
+        this();
+        this.values[0][0] = (2*near)/width;
+        this.values[1][1] = (2*near)/height;
+
+        multiply(new Matrix4(near, far));
+    }
+
 	public Matrix4 multiply(Matrix4 other) {
-		// TODO hier Matrizenmultiplikation "this = other * this" einfuegen
+		// matrix multiplication:
+        // this = other * this
+        float sum;
+        float[][] result = new float[4][4];
+
+        for (int x=0; x<4; x++) {
+            for (int y=0; y<4; y++) {
+                sum = 0.0f;
+                for (int i=0; i<4; i++) {
+                    sum += other.values[y][i] * this.values[i][x];
+                }
+                result[y][x] = sum;
+            }
+        }
+        this.values = result;
 		return this;
 	}
 
 	public Matrix4 translate(float x, float y, float z) {
-		// TODO Verschiebung um x,y,z zu this hinzufuegen
+        Matrix4 translation_matrix = new Matrix4();
+        translation_matrix.values[0][3] = x;
+        translation_matrix.values[1][3] = y;
+        translation_matrix.values[2][3] = z;
+
+        this.multiply(translation_matrix);
 		return this;
 	}
 
 	public Matrix4 scale(float uniformFactor) {
-		// TODO gleichmaessige Skalierung um Faktor "uniformFactor" zu this hinzufuegen
+        this.scale(uniformFactor, uniformFactor, uniformFactor);
 		return this;
 	}
 
 	public Matrix4 scale(float sx, float sy, float sz) {
-		// TODO ungleichfoermige Skalierung zu this hinzufuegen
+		Matrix4 scale_matrix = new Matrix4();
+        scale_matrix.values[0][0] = sx;
+        scale_matrix.values[1][1] = sy;
+        scale_matrix.values[2][2] = sz;
+
+        this.multiply(scale_matrix);
 		return this;
 	}
 
 	public Matrix4 rotateX(float angle) {
-		// TODO Rotation um X-Achse zu this hinzufuegen
+		Matrix4 rotx_matrix = new Matrix4();
+        float cos = (float)Math.cos(angle);
+        float sin = (float)Math.sin(angle);
+        rotx_matrix.values = new float[][] {
+                {1f, 0f,  0f,   0f},
+                {0f, cos, -sin, 0f},
+                {0f, sin, cos,  0f},
+                {0f, 0f,  0f,   1f}
+        };
+        this.multiply(rotx_matrix);
 		return this;
 	}
 
 	public Matrix4 rotateY(float angle) {
-		// TODO Rotation um Y-Achse zu this hinzufuegen
+        Matrix4 roty_matrix = new Matrix4();
+        float cos = (float)Math.cos(angle);
+        float sin = (float)Math.sin(angle);
+        roty_matrix.values = new float[][] {
+                {cos, 0f, -sin, 0f},
+                {0f,  1f, 0f,   0f},
+                {sin, 0f, cos,  0f},
+                {0f,  0f, 0f,   1f}
+        };
+        this.multiply(roty_matrix);
 		return this;
 	}
 
 	public Matrix4 rotateZ(float angle) {
-		// TODO Rotation um Z-Achse zu this hinzufuegen
+        Matrix4 rotz_matrix = new Matrix4();
+        float cos = (float)Math.cos(angle);
+        float sin = (float)Math.sin(angle);
+        rotz_matrix.values = new float[][] {
+                {cos, -sin, 0f, 0f},
+                {sin, cos,  0f, 0f},
+                {0f,  0f,   1f, 0f},
+                {0f,  0f,   0f, 1f}
+        };
+        this.multiply(rotz_matrix);
 		return this;
 	}
 
 	public float[] getValuesAsArray() {
-		// TODO hier Werte in einem Float-Array mit 16 Elementen (spaltenweise gefuellt) herausgeben
-		return null;
+        // return values as a 1 dimensional array (column-wise)
+        float[] result = new float[16];
+        for (int i=0; i<16; i++) {
+            int x = i/4;
+            int y = i%4;
+            result[i] = this.values[y][x];
+        }
+
+		return result;
 	}
 }
