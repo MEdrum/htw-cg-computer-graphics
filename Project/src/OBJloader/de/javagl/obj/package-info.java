a0b0ed3de1819ffbd4fff57fd7cccc7460cd957b
Fabian Backhaus <Fabian.Backhaus@Student.HTW-Berlin.de>
2026-01-17 22:49:37 +0100

    add Object Loader package

---
+++ Project/src/OBJloader/de/javagl/obj/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Classes for reading and writing Wavefront OBJ and MTL files.
+ * <p>
+ * {@link OBJloader.de.javagl.obj.Obj} objects may be created with
+ * {@link OBJloader.de.javagl.obj.Objs#create()} or by reading an OBJ file with
+ * {@link OBJloader.de.javagl.obj.ObjReader#read(java.io.InputStream)}.
+ * {@link OBJloader.de.javagl.obj.Obj} objects may be written as OBJ files with
+ * {@link OBJloader.de.javagl.obj.ObjWriter#write(ReadableObj, java.io.OutputStream)}.
+ * <br>
+ */
+package OBJloader.de.javagl.obj;
+
